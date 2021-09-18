3d20c2e49486c4c79a9e0d4b60e638f1d05eb3af
thutawunna <thutawunna@gmail.com>
2021-09-18 18:22:15 -0400

    Added Cancel feature

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ require('dotenv').config();
 
 const { WebClient, LogLevel } = require('@slack/web-api');
 const { App } = require("@slack/bolt");
+const SessionService = require('../services/SessionService');
 
 const { Wit, log } = require('node-wit');
+
+const { processDateTimeString } =require('../src/helpers');
 
 const witClient = new Wit({
     accessToken: process.env.WIT_TOKEN,
     // logger: new log.Logger(log.DEBUG)
 });
 
-// const sessionService = new SessionService();
+const sessionService = new SessionService();
 
 const client = new WebClient(process.env.SLACK_TOKEN, {
     logLevel: LogLevel.DEBUG
 });
-
-// const createSessionId = (channel, user, ts) => {
-//     return `${channel}-${user}-${ts}`;
-// }
 
 const app = new App({
     token: process.env.SLACK_TOKEN,
@@ -27,7 +26,7 @@ const app = new App({
     socketMode: true,
 });
 
-const processEvent = async (event, say, client, body) => {
+const processEvent = async (session, event, say, client, body) => {
     try {
         const queryResult = await witClient.message(event.text);
         
@@ -90,80 +89,82 @@ const processEvent = async (event, say, client, body) => {
             const dateTime = new Date(extractedEntities['datetime']);
 
             extractedEntities['datetime'] = dateTime.toISOString();
-
-            console.log(extractedEntities['datetime']);
             
             const response = await fetch('https://shielded-beach-58320.herokuapp.com/api/slack/events/get', {
-            method: 'post',
-            body: JSON.stringify({
-                slackUserID: event.user,
-                date: extractedEntities['datetime'],
-                grain: extractedEntities['datetimeGrain']
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        
-        const responseJSON = await response.json();
-        const eventList = responseJSON['events'];
-        
-        console.log(eventList);
-
-        let blockObject = [];
-        
-        if (eventList.length >= 1) {
-            eventList.forEach(event => {
-                let participants = '';
-                
-                if (event.participants) {
+                method: 'post',
+                body: JSON.stringify({
+                    slackUserID: event.user,
+                    date: extractedEntities['datetime'],
+                    grain: extractedEntities['datetimeGrain']
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            
+            const responseJSON = await response.json();
+            const eventList = responseJSON['events'];
+            
+            let blockObject = [];
+            
+            if (eventList.length >= 1) {
+                eventList.forEach(event => {
+                    let participants = '';
+                    
                     event.participants.forEach(person => {
                         participants += person;
                         if (person != event.participants[event.participants.length -1]) {
                             participants += ',';
                         }
                     });
-                } else {
-                    participants += 'None'
-                }
-                
-                
-                const eventTitle = {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": `${event.title}`
+                    
+                    const eventTitle = {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": `${event.title}`
+                        }
                     }
-                }
-
-                const startTime = new Date(event.start).toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
-                const endTime = new Date(event.end).toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
-                
-                const eventDetails = {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": `*- Date:* ${new Date(event.start).toLocaleDateString()}\n*- Time:* ${startTime} to ${endTime}\n*- Participants:* ${participants}`
+                    
+                    const eventDetails = {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*- Date:* ${new Date(event.start).toLocaleDateString()}\n*- Time:* ${new Date(event.start).toLocaleTimeString()} to ${new Date(event.end).toLocaleTimeString()}\n*- Participants:* ${participants}`
+                        }
                     }
-                }
+                    
+                    blockObject.push(eventTitle);
+                    blockObject.push({ "type": "divider" });
+                    blockObject.push(eventDetails)
+                    
+                });
+                await say({ blocks: blockObject })
                 
-                blockObject.push(eventTitle);
-                blockObject.push({ "type": "divider" });
-                blockObject.push(eventDetails)
+            } else {
+                await say('You have no events planned.');
                 
-            });
-            await say({ blocks: blockObject })
-            
-        } else {
-            await say('You have no events planned.');
-            
+            }
         }
+        
+        
+        if (extractedEntities['intent'] === 'process_event') {
+            if (extractedEntities['action'] === 'create') {
+                handleCreateEvent(extractedEntities, say);
+            } else if (extractedEntities['action'] === 'cancel') {
+                handleCancelEvent(extractedEntities, say, event.user);
+            }
+        }
+        
+        return null;
+    } catch (error) {
+        console.error(error.toString());
     }
-    
-    
-    if (extractedEntities['intent'] === 'process_event') {
-        if (extractedEntities['action'] === 'create') {
-            const meetingDescription = extractedEntities['event_type'];
+}
+
+const handleCreateEvent = async ( extractedEntities, say ) => {
+    try {
+        const meetingDescription = extractedEntities['event_type'];
 
             let participants = '';
 
@@ -223,31 +224,97 @@ const processEvent = async (event, say, client, body) => {
                     }
                 ]
             });
+    } catch (error) {
+        console.error(error.toString());
+    }
+}
+
+const handleCancelEvent = async ( extractedEntities, say, userID ) => {
+    try {
+        const response = await fetch('https://shielded-beach-58320.herokuapp.com/api/slack/events/get', {
+            method: 'post',
+            body: JSON.stringify({
+                slackUserID: userID,
+                date: extractedEntities['datetime'],
+                grain: extractedEntities['datetimeGrain'],
+                eventName: extractedEntities['event_type']
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const responseJSON = await response.json();
+        const eventList = responseJSON['events'];
+
+        console.log(`Event List: ${eventList}`);
+
+        if (eventList.length == 1) {
+            const event = eventList[0];
+            
+            const eventStartTime = new Date(event.start);
+
+            console.log(`Event: ${event}`)
+            await say({
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*Would you like to cancel this event?* \n\n*Appointment Date:* ${eventStartTime.toLocaleString()}\n*Appointment Type:* ${event.title}\n*Participants:* ${event.participants || 'None'}`
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "block_id": "verify_appointment",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Cancel"
+                                },
+                                "style": "danger",
+                                "action_id": "confirm_appointment_cancel",
+                                "value": `${eventStartTime.toISOString()}|${event.title}`
+                            },
+                            // {
+                            //     "type": "button",
+                            //     "text": {
+                            //         "type": "plain_text",
+                            //         "text": "Cancel"
+                            //     },
+                            //     "style": "danger",
+                            //     "action_id": "cancel_appointment_cancel",
+                            //     "value": "Cancel"
+                            // }
+                        ]
+                    }
+                ]
+            });
         }
+
+    } catch (error) {
+        console.error(error.toString());
     }
     
-    return null;
-} catch (error) {
-    console.error(error.toString());
-}
 }
 
 const handleMessage = async (event, say, client, body) => {
-    // const sessionId = createSessionId(event.channel, event.user, event.thread_ts || event.ts);
-    // let session = sessionService.get(sessionId);
-    // if (!session) {
-    //     session = sessionService.create(sessionId);
-    //     session.context = {
-    //         slack: {
-    //             channel: event.channel,
-    //             user: event.user,
-    //             thread_ts: event.thread_ts || event.ts
-    //         },
-    //     };
-    // }
+    const sessionId = createSessionId(event.channel, event.user, event.thread_ts || event.ts);
+    let session = sessionService.get(sessionId);
+    if (!session) {
+        session = sessionService.create(sessionId);
+        session.context = {
+            slack: {
+                channel: event.channel,
+                user: event.user,
+                thread_ts: event.thread_ts || event.ts
+            },
+        };
+    }
     // console.log(session);
     
-    let action = await processEvent(event, say, client, body);
+    let action = await processEvent(session, event, say, client, body);
     
 }
 
@@ -292,7 +359,7 @@ app.action('connect_slack_calendar_app', async ({ action, ack, body, say }) => {
     });
     
     if (response.status === 200) {
-        await say('Slack Account Connected. Please verify by checking your account settings at http://localhost:3000/verify/slack');
+        await say('Slack Account Connected. Please verify by checking your account settings at https://shielded-beach-58320.herokuapp.com/verify/slack');
     } else {
         const responseJSON = await response.json();
         await say(`${responseJSON.message}`);
@@ -339,6 +406,38 @@ app.action('confirm_appointment_create', async ({ body, action, ack, say }) => {
     } catch (error) {
         console.error(error.toString());
     }
+});
+
+app.action('confirm_appointment_cancel', async ({ body, action, ack, say }) => {
+    const user = body.user;
+
+    await ack();
+
+    const payload = action['value'].split('|');
+
+    const eventStart = payload[0];
+    const title = payload[1];
+
+    const response = await fetch('https://shielded-beach-58320.herokuapp.com/api/slack/events/cancel/', {
+        method: 'post',
+        body: JSON.stringify({
+            slackID: user.id,
+            eventStart: eventStart,
+            eventTitle: title
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.status === 200) {
+        await say(`The ${title} has been cancelled`);
+    } else {
+        const responseJSON = await response.json();
+        await say(`${responseJSON.message}`);
+    }
+
+
 });
 
 app.action('cancel_appointment_create', async ({ ack, say }) => {
